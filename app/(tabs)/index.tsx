8c1ddd3120cd341e538ea05d9e2f025e1464a774
Park Sang Yong <rmarkd2212@naver.com>
2026-04-02 16:40:48 +0900

    feature:nav bottom 작업

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,30 @@
-import { Redirect } from "expo-router";
+import { ScrollView, StyleSheet } from "react-native";
+
+import CategoryRow from "@/components/home/CategoryRow";
+import NearbyShopSection from "@/components/home/NearbyShopSection";
+import PopularShopSection from "@/components/home/PopularShopSection";
+import RecentReviewSection from "@/components/home/RecentReviewSection";
+import SearchBar from "@/components/home/SearchBar";
 
 export default function Index() {
-  return <Redirect href="/(tabs)/home" />;
+  return (
+    <ScrollView style={styles.container}>
+      <SearchBar />
+
+      <CategoryRow />
+
+      <NearbyShopSection />
+
+      <PopularShopSection />
+
+      <RecentReviewSection />
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
