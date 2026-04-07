3a1c7399f5c6efa45765778c74bfe81cc20882e4
park.youngkyu <yk.park@nuriggum.com>
2026-04-07 14:07:13 +0900

    feat(home): home 코드 리팩토링

--- components/home/PopularShopSection.tsx
+++ components/home/PopularShopSection.tsx
@@ -1,32 +1,27 @@
-import { StyleSheet, View } from "react-native";
-import AppText from "../common/AppText";
-import ShopCardVertical from "./ShopCardVertical";
+import { ScrollView, StyleSheet, View } from "react-native";
+
+import { MOCK_SHOPS } from "@/data/mockShops";
+
+import SectionHeading from "./SectionHeading";
+import ShopCardHorizontal from "./ShopCardHorizontal";
 
 export default function PopularShopSection() {
   return (
     <View style={styles.container}>
-      <AppText style={styles.title}>인기 샵</AppText>
+      <SectionHeading style={styles.heading}>인기 샵</SectionHeading>
 
-      <ShopCardVertical
-        name="펫살롱 강남점"
-        rating={4.9}
-        distance="1.2km"
-        image="https://images.unsplash.com/photo-1548199973-03cce0bbc87b"
-      />
-
-      <ShopCardVertical
-        name="댕댕이 미용실"
-        rating={4.8}
-        distance="2.0km"
-        image="https://images.unsplash.com/photo-1601758228041-f3b2795255f1"
-      />
-
-      <ShopCardVertical
-        name="꼬모 애견미용"
-        rating={4.7}
-        distance="2.8km"
-        image="https://images.unsplash.com/photo-1516734212186-a967f81ad0d7"
-      />
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {MOCK_SHOPS.map((shop) => (
+          <ShopCardHorizontal
+            key={shop.id}
+            name={shop.name}
+            rating={shop.rating}
+            reviewCount={shop.reviewCount}
+            distance={shop.distance}
+            image={shop.image}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -37,8 +32,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
 
-  title: {
-    fontSize: 18,
-    fontFamily: "Poppins_600SemiBold",
+  heading: {
+    marginBottom: 10,
   },
 });
